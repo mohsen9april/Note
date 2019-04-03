d8e428a5571d433fea397fa657d19745725eb669
Mohsen Abdollahi <mohsenabdollahi@Mohsens-Mac.local>
2019-04-03 11:57:48 +0430

    Add TableView & AddButton to Create a List

--- Note/ViewController.swift
+++ Note/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    var name = [String]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        
+        let alert  = UIAlertController(title: "Add New Title", message: "You Can Add a Name For Your Title", preferredStyle: .alert)
+        alert.addTextField { (textFiled: UITextField) -> Void in
+        }
+        let saveAction = UIAlertAction(title: "Save", style: .default) { (action: UIAlertAction) -> Void in
+            let textField = alert.textFields!.first
+            self.name.append(textField!.text!)
+            self.tableView.reloadData()
+        }
+        alert.addAction(saveAction)
+        present(alert, animated: true, completion: nil)
+        
+ 
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = name[indexPath.row]
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
